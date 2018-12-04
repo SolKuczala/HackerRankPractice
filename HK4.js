c50b6657dd19ee488ba0c036c55c0a0230bd64f1
SolKuczala <sol.kuczala@gmail.com>
2018-12-04 16:09:29 -0300

    done

---
+++ HK4.js
@@ -0,0 +1,16 @@
+/// staircase
+function staircase(n) {
+    let x = []
+    for (let i = 0; i < n; i++){
+         x.push(' ')
+    }
+    
+    for (let j = 0; j < n; j++) {
+        x.push('#');
+        x.shift();
+        console.log(x.join(' '));
+        
+        
+    }
+ }
+ console.log(staircase(6));
