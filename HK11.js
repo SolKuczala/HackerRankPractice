c86774dd9a12fddde8968a903b768c75e1349f06
SolKuczala <sol.kuczala@gmail.com>
2018-12-11 18:41:24 -0300

    countApplesAndOranges

--- HK11.js
+++ HK11.js
@@ -3,17 +3,18 @@ function countApplesAndOranges(s, t, a, b, apples, oranges) {
     let naranjasAdentro = 0
     apples.map(e => {
         let posicionada=e + a
-        if (posicionada > 6 && posicionada < 12) {
+        if (posicionada >=s && posicionada <= t) {
             manzanasAdentro++
         }
     })
     oranges.map(e => {
         let posicionada2= e + b
-        if (posicionada2 > 6 && posicionada2 < 12) {
+        if (posicionada2 >= s && posicionada2 <= t) {
             naranjasAdentro++
         }
     })
-   return [manzanasAdentro, naranjasAdentro]
+   console.log(manzanasAdentro, naranjasAdentro);
+   
     
 }
 console.log(countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6]));
